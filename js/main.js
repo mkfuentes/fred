5c795b1779ded6cbc198cbb95d6d04a16310ea96
mk fuentes <miguelangel.fm@outlook.com>
2020-01-08 14:45:21 -0600

    Push game.currentGame

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var game = {
+  possibilities: [
+    "zorro",
+    "caballo",
+    "gallina",
+    "panda",
+    "rana",
+    "mapache",
+    "unicornio",
+    "perro",
+    "conejo"
+  ],
+  currentGame: [],
+  points: 0
+};
+
+/* var random = Math.random();
+var multiplicacion = random * 9;
+var floor = Math.floor(multiplicacion);
+console.log(`Número random: ${random}`);
+console.log(`Random multiplicado: ${multiplicacion}`);
+console.log(`Posición del arreglo: ${floor}`);
+game.currentGame.push(game.possibilities[floor]);
+console.log("possibilities", game.possibilities);
+console.log("currentGame", game.currentGame);
+//alert(game.currentGame.length);
+console.log(game.currentGame.length);
+ */
+
+function uno() {
+
+    var random = Math.random();
+    var multiplicacion = random * 9;
+    var floor = Math.floor(multiplicacion);
+    var actualPosition = game.possibilities[floor];
+
+console.log(`Número random: ${random}`);
+console.log(`Random multiplicado: ${multiplicacion}`);
+console.log(`Posición del arreglo: ${floor}`);
+console.log(actualPosition, "es el actual");
+console.log(typeof actualPosition);
+
+  var itemActual = document.getElementById(actualPosition);
+  itemActual.classList.add("circleColor");
+  setTimeout(function() {
+    itemActual.classList.remove("circleColor");
+  }, 500);
+
+  console.log("possibilities", game.possibilities);
+console.log("currentGame, actual:", game.currentGame);
+//alert(game.currentGame.length);
+
+
+  return game.currentGame.push(actualPosition),
+  console.log("nivel:", game.currentGame.length);;
+}
+
+uno();uno();
