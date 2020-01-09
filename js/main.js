525959dea48c15b0257df11bb187bc1ca21c7c79
mk fuentes <miguelangel.fm@outlook.com>
2020-01-09 12:36:57 -0600

    Ultra ultimate

--- js/main.js
+++ js/main.js
@@ -1,72 +1,129 @@
 var game = {
-  possibilities: [
-    "zorro",
-    "caballo",
-    "gallina",
-    "panda",
-    "rana",
-    "mapache",
-    "unicornio",
-    "perro",
-    "conejo"
-  ],
-  currentGame: [],
-  player:[],
-  points: 0
-};
-
-/* var random = Math.random();
-var multiplicacion = random * 9;
-var floor = Math.floor(multiplicacion);
-console.log(`Número random: ${random}`);
-console.log(`Random multiplicado: ${multiplicacion}`);
-console.log(`Posición del arreglo: ${floor}`);
-game.currentGame.push(game.possibilities[floor]);
-console.log("possibilities", game.possibilities);
-console.log("currentGame", game.currentGame);
-//alert(game.currentGame.length);
-console.log(game.currentGame.length);
- */
-
-function uno() {
-
-    var floor = Math.floor(Math.random()*9);
-    var actualPosition = game.possibilities[floor];
-
-//console.log(`Número random: ${random}`);
-//console.log(`Random multiplicado: ${multiplicacion}`);
-//console.log(`Posición del arreglo: ${floor}`);
-//console.log(actualPosition, "es el actual");
-
-  var itemActual = document.getElementById(actualPosition);
-  itemActual.classList.add("circleColor");
-  setTimeout(function() {
-    itemActual.classList.remove("circleColor");
-  }, 500);
-
-  //console.log("possibilities", game.possibilities);
-//console.log("currentGame, actual:", game.currentGame);
-//alert(game.currentGame.length);
-
-
-  return game.currentGame.push(actualPosition),
-  console.log("nivel:", game.currentGame.length),
-  console.log(game.currentGame);
-}
-
-uno();
-
-function addToPlayer(id){
-    var i = id
-    game.player.push(i)
-    compare()
-}
-
-function compare(){
-    if(JSON.stringify(game.player) == JSON.stringify(game.currentGame)){
-        return console.log("bien")
-    }else{
-            return console.log("mal")
-
+    count: 0,
+    possibilities: [
+      "zorro",
+      "caballo",
+      "gallina",
+      "panda",
+      "rana",
+      "mapache",
+      "unicornio",
+      "perro",
+      "conejo"
+    ],
+    currentGame: [],
+    player: [],
+    strict: false
+  };
+  
+  function clearGame() {
+    game.currentGame = [];
+    game.count = 0;
+    addCount();
+  }
+  
+  function newGame() {
+    clearGame();
+  }
+  
+  function showMoves(){ 
+    var i = 0;
+    var moves = setInterval(function() {
+      playGame(game.currentGame[i]);
+      let b = document.getElementById(game.currentGame[i])
+      b.classList.add("circleColor")
+      setTimeout(function() {
+        b.classList.remove("circleColor")
+      }, 600)
+      i++;
+      if (i >= game.currentGame.length) {
+        clearInterval(moves);
+      }
+    }, 1000);
+    clearPlayer()
+  }
+  
+  function playGame(field) {
+    $(field).addClass("circleColor");
+    setTimeout(function() {
+      $(field).removeClass("circleColor");
+    }, 1000);
+  }
+  
+  function clearPlayer() {
+    game.player = [];
+  }
+  
+  function addToPlayer(id) {
+    var field = id;
+    console.log("field", field);
+    game.player.push(field);
+    playerTurn(field);
+  }
+  
+  function playerTurn(x) {
+    if (
+      game.player[game.player.length - 1] !==
+      game.currentGame[game.player.length - 1]
+    ) {
+      if (game.strict) {
+        alert("Se reinicia de cero");
+        newGame();
+      } else {
+        alert("Intentar de nuevo");
+        showMoves();
+      }
+    } else {
+      console.log("Bien");
+      var check = game.player.length == game.currentGame.length;
+      if (check) {
+        if (game.count == 20) {
+          alert("You won! Congrats.");
+        } else {
+          alert("Next round!");
+          nextLevel();
         }
+      }
     }
+  }
+  
+  function nextLevel() {
+    addCount();
+  }
+  
+  function generateMove() {
+    var random = Math.random();
+    var multiplicacion = random * 9;
+    var floor = Math.floor(multiplicacion);
+    console.log(`Número random: ${random}`);
+    console.log(`Random multiplicado: ${multiplicacion}`);
+    console.log(`Posición del arreglo: ${floor}`);
+  
+    game.currentGame.push(game.possibilities[floor]);
+    console.log(game.possibilities);
+    console.log(game.currentGame);
+    //alert(game.currentGame.length);
+    console.log(game.currentGame.length);
+    showMoves();
+  }
+  
+  function addCount() {
+    //score
+    game.count++;
+    $("#clickNumber").addClass("animated fadeOutDown");
+    setTimeout(function() {
+      $("#clickNumber")
+        .removeClass("fadeOutDown")
+        .html(game.count)
+        .addClass("fadeInDown");
+    }, 200);
+  
+    generateMove();
+  }
+  
+  newGame();
+  
+  function resetGame (){
+    game.strict = true
+    newGame()
+  }
